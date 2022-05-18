112a5a602fe1c127570c10e161ae4e911604666d
Your Name <you@example.com>
2022-05-18 14:43:56 +0300

    Created TextFields

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -38,6 +40,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var textInput = TextEditingController();
+    var amountInput = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -54,18 +59,87 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Card(
             child: Text("Chart"),
           ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 10,
+                  ),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Title',
+                    ),
+                    controller: textInput,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 10,
+                  ),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Amount',
+                    ),
+                    controller: amountInput,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    right: 10,
+                  ),
+                  child: RaisedButton(
+                    onPressed: () {
+                      print(amountInput.text);
+                      print(textInput.text);
+                    },
+                    child: const Text("Add Tx"),
+                  ),
+                ),
+              ],
+            ),
+          ),
           Column(
             children: transactions.map(
               (tx) {
-                return ListTile(
-                  leading: Text(tx.title),
-                  title: Text(tx.amount.toString()),
-                  subtitle: Text(tx.date.toString()),
+                return Card(
+                  elevation: 3,
+                  child: ListTile(
+                    leading: Container(
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        width: 2,
+                        color: Colors.blue,
+                      )),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      tx.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMEd().format(tx.date),
+                    ),
+                  ),
                 );
               },
             ).toList(),
