faead12b271e2f988a9a284f4939349a65975b65
Your Name <you@example.com>
2022-05-18 13:28:04 +0300

    Created Category List and build sample layout of lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,15 +9,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  // const HomePage({Key? key}) : super(key: key);
+
+  List<Transaction> transactions = [
+    Transaction(
+      title: "Food",
+      amount: 35.5,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      title: "Transport",
+      amount: 10.5,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      title: "Utility",
+      amount: 55.5,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Expense",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
         ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white10,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Card(
+            child: Text("Chart"),
+          ),
+          Column(
+            children: transactions.map(
+              (tx) {
+                return ListTile(
+                  leading: Text(tx.title),
+                  title: Text(tx.amount.toString()),
+                  subtitle: Text(tx.date.toString()),
+                );
+              },
+            ).toList(),
+          )
+        ],
       ),
     );
   }
---
+++ lib/transaction.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Transaction {
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+}
